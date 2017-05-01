f6d851737e754ea9d95385c9423903dcc4420d4a
radswiat <rad.swiat@gmail.com>
2017-05-01 22:32:54 +0100

    Connect apollo with component

--- client/src/pages/manage-categories/manage-categories.jsx
+++ client/src/pages/manage-categories/manage-categories.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CSSModules from 'react-css-modules';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import css from './scss/manage-categories.scss';
 
 /**
  * React app class
  */
-@CSSModules(css)
-export default class ManageCategories extends React.Component {
+export class ManageCategories extends React.Component {
+
+  static propTypes = {
+    data: PropTypes.shape({
+      loading: PropTypes.bool,
+      error: PropTypes.object,
+      Trainer: PropTypes.object
+    }).isRequired
+  };
+
+  static qgl = gql`
+    {
+      getCategoryById(categoryId: 5) {
+        title,
+        description,
+        parent
+      }
+    }
+  `;
 
   render() {
     return (
@@ -17,3 +37,9 @@ export default class ManageCategories extends React.Component {
     );
   }
 }
+
+
+// console.error(' ----- graphql query -----');
+// console.warn(TrainerQuery);
+//
+export default graphql(ManageCategories.qgl)(ManageCategories);
